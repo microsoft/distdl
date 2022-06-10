1a6cfe7ea8d69302d1eeb321e048273ad9a23b47
v-asojoodi <amir@asojoodimpitorch.gr04bzi5ka5uphweb4livk5v2f.jx.internal.cloudapp.net>
2022-06-10 22:02:20 +0000

    Init: update running script, DistDL working verified

--- examples/co2flow/run_container.sh
+++ examples/co2flow/run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --gpus all -v /home/pwitte:/workspace/home -e SLEIPNER_CREDENTIALS=$SLEIPNER_CREDENTIALS -it distdl:v1.0
+docker run --gpus all -v /scratch/amir:/workspace/home -e SLEIPNER_CREDENTIALS=$SLEIPNER_CREDENTIALS -it distdl:v1.0
