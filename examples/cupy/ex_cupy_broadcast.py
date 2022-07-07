91c5c737c8f647c83ce19a6ea9ddeff9b416526a
v-asojoodi <v-asojoodi@microsoft.com>
2022-07-07 23:52:58 +0000

    Dev: test feeding torch tensors to mpi4py directly

--- examples/cupy/ex_cupy_broadcast.py
+++ examples/cupy/ex_cupy_broadcast.py
@@ -1,22 +1,27 @@
 from mpi4py import MPI
+import torch
 import cupy as cp
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-with cp.cuda.Device(rank):
+# with cp.cuda.Device(rank):
 
-    # Create arrays on gpu
-    if rank == 0:
-        data = cp.ones((100,100,100))
-        print("Source data from rank {} on device {}".format(rank, data.device))
-    else:
-        data = cp.empty((100,100,100))
-        print("Destination on rank {} on device {}".format(rank, data.device))
+# Create arrays on gpu
+if rank == 0:
+    # data = cp.ones((100,100,100))
+    data = torch.ones(1000, device="cuda:0")
+    print("Source data from rank {} on device {}".format(rank, data.device))
+else:
+    # data = cp.empty((100,100,100))
+    data = torch.empty(1000, device="cuda:0")
+    print("Destination on rank {} on device {}".format(rank, data.device))
 
 
-    # Make sure GPU buffer is ready
-    cp.cuda.get_current_stream().synchronize()
+# Make sure GPU buffer is ready
+# cp.cuda.get_current_stream().synchronize()
 
-    # Broadcast data
-    comm.Bcast(data, root=0)
+# Broadcast data
+comm.Bcast(data, root=0)
+
+print(f"From rank {rank} First value: ", data[0])
