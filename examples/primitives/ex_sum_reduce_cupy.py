95bd214c92a7b7ed091f0535ba7edba340ed0234
v-asojoodi <v-asojoodi@microsoft.com>
2022-08-05 02:23:29 +0000

    Dev: replace cupy with torch, weird invalid context bug resolved

--- examples/primitives/ex_sum_reduce_cupy.py
+++ examples/primitives/ex_sum_reduce_cupy.py
@@ -23,6 +23,10 @@ P_world._comm.Barrier()
 torch.cuda.set_device(P_world.rank % torch.cuda.device_count())
 P_world.device = torch.cuda.current_device()
 
+# device = torch.cuda.device(P_world.rank % torch.cuda.device_count())
+# torch.cuda.set_device(device)
+# print(f"P_world.rank: {P_world.rank}, P_world.device = {P_world.device}, cur_dev: {torch.cuda.current_device()}")
+
 # Create the input/output partition (using the first 2 workers)
 in_shape = (2, 3)
 in_size = np.prod(in_shape)
@@ -39,7 +43,7 @@ out_workers = np.arange(P_world.size-out_size, P_world.size)
 P_y_base = P_world.create_partition_inclusive(out_workers)
 P_y = P_y_base.create_cartesian_topology_partition(out_shape)
 
-x_global_shape = np.array([6, 6])
+x_global_shape = np.array([5, 6])
 
 # Setup the input tensor.  Any worker in P_x will generate its part of the
 # input tensor.  Any worker not in P_x will have a zero-volume tensor.
@@ -62,8 +66,8 @@ if P_x.active:
                                              x_global_shape)
     ## x = np.zeros(x_local_shape) + P_x.rank + 1
     ## x = torch.from_numpy(x)
-    x = torch.zeros(*x_local_shape, device=x.device) + (P_x.rank + 1)
-    
+    x = torch.zeros(*x_local_shape, device=P_x.device) + (P_x.rank + 1)
+
 x.requires_grad = True
 print(f"rank {P_world.rank}; index {P_x.index}; value {x}")
 
@@ -97,15 +101,18 @@ y_global_shape = assemble_global_tensor_structure(y, P_y).shape
 ## dy = zero_volume_tensor(device=cp.cuda.runtime.getDevice())
 dy = zero_volume_tensor(device=P_y.device)
 
+# print(f"P_y.rank: {P_y.rank}, P_y.device: {P_y.device}, P_x.rank: {P_x.rank}, P_x.device: ",
+#       f"{P_x.device}, P_world.rank: {P_world.rank}, P_world.device: {P_world.device}\n")
+
 if P_y.active:
     y_local_shape = slicing.compute_subshape(P_y.shape,
                                              P_y.index,
                                              y_global_shape)
     ## dy = np.zeros(y_local_shape) + P_y.rank + 1
     ## dy = torch.from_numpy(dy)
-    dy = torch.zeros(*y_local_shape, device=dy.device) + (P_y.rank + 1)
-    
-    
+    dy = torch.zeros(*y_local_shape, device=P_y.device) + (P_y.rank + 1)
+
+
 print(f"rank {P_world.rank}; index {P_y.index}; value {dy}")
 
 # Apply the adjoint of the layer.
