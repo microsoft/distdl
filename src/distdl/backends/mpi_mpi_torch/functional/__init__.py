3871b4b662caae205aaa046eb3461262555c501c
v-asojoodi <v-asojoodi@microsoft.com>
2022-08-11 21:40:54 +0000

    Fix: replace _cupy with _torch in imports

--- src/distdl/backends/mpi_mpi_torch/functional/__init__.py
+++ src/distdl/backends/mpi_mpi_torch/functional/__init__.py
@@ -1,11 +1,11 @@
-from distdl.backends.mpi_mpi_cupy.functional.all_sum_reduce_cupy import AllSumReduceFunction  # noqa: F401
-from distdl.backends.mpi_mpi_cupy.functional.broadcast_cupy import BroadcastFunction  # noqa: F401
-from distdl.backends.mpi_mpi_cupy.functional.halo_exchange_cupy import HaloExchangeFunction  # noqa: F401
-from distdl.backends.mpi_mpi_cupy.functional.repartition_cupy import RepartitionFunction  # noqa: F401
-from distdl.backends.mpi_mpi_cupy.functional.sum_reduce_cupy import SumReduceFunction  # noqa: F401
+from distdl.backends.mpi_mpi_torch.functional.all_sum_reduce import AllSumReduceFunction  # noqa: F401
+from distdl.backends.mpi_mpi_torch.functional.broadcast import BroadcastFunction  # noqa: F401
+from distdl.backends.mpi_mpi_torch.functional.halo_exchange import HaloExchangeFunction  # noqa: F401
+from distdl.backends.mpi_mpi_torch.functional.repartition import RepartitionFunction  # noqa: F401
+from distdl.backends.mpi_mpi_torch.functional.sum_reduce import SumReduceFunction  # noqa: F401
 
-from . import all_sum_reduce_cupy  # noqa: F401
-from . import broadcast_cupy  # noqa: F401
-from . import halo_exchange_cupy  # noqa: F401
-from . import repartition_cupy  # noqa: F401
-from . import sum_reduce_cupy  # noqa: F401
+from . import all_sum_reduce  # noqa: F401
+from . import broadcast  # noqa: F401
+from . import halo_exchange  # noqa: F401
+from . import repartition  # noqa: F401
+from . import sum_reduce  # noqa: F401
