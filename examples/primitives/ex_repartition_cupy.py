70bd9ed3139e044af59208d1ab5aba1ffe3bcea2
v-asojoodi <v-asojoodi@microsoft.com>
2022-08-11 23:47:44 +0000

    Fix: correct imports in primitives examples

--- examples/primitives/ex_repartition_cupy.py
+++ examples/primitives/ex_repartition_cupy.py
@@ -10,8 +10,8 @@ import torch
 from mpi4py import MPI
 
 import distdl.utilities.slicing as slicing
-from distdl.backends.mpi.partition import MPIPartition
-from distdl.nn.repartition_cupy import Repartition
+from distdl.backends.mpi_mpi_cupy.partition import MPIPartition
+from distdl.nn.repartition import Repartition
 from distdl.utilities.torch import zero_volume_tensor
 
 
