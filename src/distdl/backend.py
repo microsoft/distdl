bc7f53f6f8eed4f2af39cb1d92b14ee423fefd1e
v-asojoodi <v-asojoodi@microsoft.com>
2022-08-16 04:31:37 +0000

    Dev: set backend based on environment variables

--- src/distdl/backend.py
+++ src/distdl/backend.py
@@ -1,3 +1,24 @@
-import distdl.backends.mpi
+import os
+import distdl.backends.mpi_mpi_numpy
+import distdl.backends.mpi_mpi_cupy
+import distdl.backends.mpi_mpi_torch
 
-backend = distdl.backends.mpi
+backend = distdl.backends.mpi_mpi_numpy
+
+try:
+    if os.environ["DISTDL_BACKEND"] == "Cupy":
+        backend = distdl.backends.mpi_mpi_cupy
+        print("---- Using Cupy ----")
+    elif os.environ["DISTDL_BACKEND"] == "Numpy":
+        backend = distdl.backends.mpi_mpi_numpy
+        print("---- Using Numpy ----")
+    elif os.environ["DISTDL_BACKEND"] == "Torch":
+        backend = distdl.backends.mpi_mpi_torch
+        print("---- Using Torch ----")
+    else:
+        backend = distdl.backends.mpi_mpi_numpy
+        print("---- No backends specified, using Numpy for now ----")
+      
+except:
+    backend = distdl.backends.mpi_mpi_numpy
+    print("---- Invalid Backend. Using Numpy for now. ----")
