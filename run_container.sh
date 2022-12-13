bfecea667edf054beb10f9f1041f4fd686d61704
Ubuntu <azureuser@rjh-dev.vslrq2glkt5uzkgx1acyd0ijfg.bx.internal.cloudapp.net>
2022-12-13 23:42:30 +0000

    fix container runner

--- run_container.sh
+++ run_container.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker run --privileged=true --gpus all -v /home/azureuser:/workspace/home -it distdl:v2.0
-
+docker run --privileged=true \
+    --gpus all --sig-proxy=false --cap-add=ALL \
+    --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+    -v /home/azureuser:/workspace/home -it distdl:v2.0
