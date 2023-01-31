7fb6543216eb1e813d7df4ad6fb33175fb3cd0db
Ubuntu <azureuser@aise-workloads-c000002.3wh2gfgj1g4u3fzfinvbjk4yic.bx.internal.cloudapp.net>
2023-01-31 19:11:46 +0000

    update

--- run_container.sh
+++ run_container.sh
@@ -3,4 +3,6 @@
 docker run --privileged=true \
     --gpus all --sig-proxy=false --cap-add=ALL \
     --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-    -v /home/azureuser:/workspace/home -it distdl:v2.0
+    -v /home/azureuser/pwitte:/workspace/home \
+    -v /shared/pwitte:/shared \
+    -it distdl:v1.0
