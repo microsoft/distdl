61c142b7e6c0e3b4ed64a24b051f7e9c812357b7
Philipp Witte <pwitte@microsoft.com>
2023-02-20 21:40:05 +0000

    feat (#2359) check input changed optional

--- src/distdl/config.py
+++ src/distdl/config.py
@@ -5,6 +5,7 @@ import distdl.backends
 # Environment variable names
 BACKEND_COMM_ENV = "DISTDL_BACKEND_COMM"
 BACKEND_ARRAY_ENV = "DISTDL_BACKEND_ARRAY"
+PRE_HOOK_CHECK_INPUT_CHANGED_ENV = "DISTDL_CHECK_INPUT_CHANGED"
 
 # Get default communication protocol
 def get_default_comm():
@@ -39,14 +40,27 @@ def get_default_array():
         backend_array = "numpy"
     return backend_array
 
+def get_default_pre_hook_setting():
+    supported_settings = ['0', '1', 'False', 'True']
+    if PRE_HOOK_CHECK_INPUT_CHANGED_ENV in os.environ:
+        if os.environ[PRE_HOOK_CHECK_INPUT_CHANGED_ENV] in supported_settings:
+            check_input_changed = bool(int(os.environ[PRE_HOOK_CHECK_INPUT_CHANGED_ENV]))
+        else:
+            logger.warning("Specified setting for check input changed does not exist. Default to False.")
+    else:
+        check_input_changed = False
+    return check_input_changed
 
-def set_backend(backend_comm=None, backend_array=None):
+def set_backend(backend_comm=None, backend_array=None, check_input_changed=None):
 
     # Get default config
     if backend_comm is None:
         backend_comm = get_default_comm()
     if backend_array is None:
         backend_array = get_default_array()
+    if check_input_changed is None:
+        check_input_changed = get_default_pre_hook_setting()
+    distdl.config.check_input_changed = check_input_changed
 
     backend_config = '_'.join([backend_comm, backend_array])
 
