4d6ffbe322ea3ad8770d8a31a5273e4d86589aa0
Philipp Witte <pwitte@microsoft.com>
2023-04-20 21:26:42 +0000

    remove obsolete docker config

--- run_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --privileged=true \
-    --gpus all --sig-proxy=false --cap-add=ALL \
-    --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-    -v /home/azureuser/pwitte:/workspace/home \
-    -v /shared/pwitte:/shared \
-    -it distdl:v1.0
