3d5b7546bd921933ed7642b27a28b717f1a66d51
Philipp Witte <pwitte@microsoft.com>
2023-10-18 16:59:10 +0000

    bug(#47) return cupy dtype instead of torch

--- src/distdl/backends/common/convert.py
+++ src/distdl/backends/common/convert.py
@@ -5,11 +5,11 @@ import torch
 import distdl.backends as backends
 
 
-cupy_backends = ["mpi_cupy", "nccl_cupy"]
-
 def convert_torch_to_model_dtype(dtype):
-    if backends.backend.__name__ in cupy_backends:
+    if backends.backend.__name__ == "nccl_cupy":
         return dtype
+    elif backends.backend.__name__ == "mpi_cupy":
+        return dtype_utils.torch_to_cupy_dtype_dict[dtype]
     elif backends.backend.__name__ == "mpi_numpy":
         return dtype_utils.torch_to_numpy_dtype_dict[dtype]
     else:
@@ -17,8 +17,10 @@ def convert_torch_to_model_dtype(dtype):
 
 
 def convert_model_to_torch_dtype(dtype):
-    if backends.backend.__name__ in cupy_backends:
+    if backends.backend.__name__ == "nccl_cupy":
         return dtype
+    elif backends.backend.__name__ == "mpi_cupy":
+        return dtype_utils.cupy_to_torch_dtype_dict[dtype]
     elif backends.backend.__name__ == "mpi_numpy":
         return dtype_utils.numpy_to_torch_dtype_dict[dtype]
     else:
@@ -26,8 +28,10 @@ def convert_model_to_torch_dtype(dtype):
 
 
 def convert_intID_to_model_dtype_dict(intID):
-    if backends.backend.__name__ in cupy_backends:
+    if backends.backend.__name__ == "nccl_cupy":
         return dtype_utils.intID_to_torch_dtype_dict[intID]
+    elif backends.backend.__name__ == "mpi_cupy":
+        return dtype_utils.intID_to_cupy_dtype_dict[intID]
     elif backends.backend.__name__ == "mpi_numpy":
         return dtype_utils.intID_to_numpy_dtype_dict[intID]
     else:
@@ -35,8 +39,10 @@ def convert_intID_to_model_dtype_dict(intID):
 
 
 def convert_model_to_intID_dtype_dict(dtype):
-    if backends.backend.__name__ in cupy_backends:
+    if backends.backend.__name__ == "nccl_cupy":
         return dtype_utils.torch_to_intID_dtype_dict[dtype]
+    elif backends.backend.__name__ == "mpi_cupy":
+        return dtype_utils.cupy_to_intID_dtype_dict[dtype]
     elif backends.backend.__name__ == "mpi_numpy":
         return dtype_utils.numpy_to_intID_dtype_dict[dtype]
     else:
